9535c30ae7442c87887bf7fdc61b3b629026ba44
Anton <gluxow.anton2015@yandex.ru>
2024-10-13 17:29:45 +0300

    Создан класс контекста бд для приложения

---
+++ Persistence/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public sealed class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions opt): base(opt) {
+            Database.EnsureCreated();
+        }
+
+        //DbSet
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.ApplyConfigurationsFromAssembly(AssemblyReference.Assembly);
+        }
+    }
+}
---
+++ Persistence/AssemblyReference.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+namespace Persistence
+{
+    public sealed class AssemblyReference
+    {
+        public static readonly Assembly Assembly =  typeof(AssemblyReference).Assembly;
+    }
+}
