889acea184ddbf8a85554ff668ced4186b714f39
Anton <gluxow.anton2015@yandex.ru>
2024-10-13 18:00:32 +0300

    Добавлена сущность User, добавлена в контекст бд

---
+++ Domain/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Enums;
+using Domain.Primitives;
+
+namespace Domain.Entities
+{
+    public sealed class User : Entity
+    {
+        public string UserName { get; private set; }
+        public string Email { get; private set; }
+        public SystemRole SystemRole { get; private set; }
+        public UserRole UserRole { get; private set; }
+        public string HashPassword { get; private set; }
+
+        public User(Guid id) : base(id) {
+        }
+    }
+}
--- Persistence/ApplicationDbContext.cs
+++ Persistence/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Persistence
 {
@@ -8,7 +9,7 @@ namespace Persistence
             Database.EnsureCreated();
         }
 
-        //DbSet
+        public DbSet<User> Users { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder) {
             modelBuilder.ApplyConfigurationsFromAssembly(AssemblyReference.Assembly);
