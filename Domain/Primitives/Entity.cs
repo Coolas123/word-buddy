56bbcdd118484577212043911037d38d0ba69048
Anton <gluxow.anton2015@yandex.ru>
2024-10-13 17:42:09 +0300

    Создан примитивный класс Entity

---
+++ Domain/Primitives/Entity.cs
@@ -0,0 +1,43 @@
+﻿namespace Domain.Primitives
+{
+    public abstract class Entity : IEquatable<Entity>
+    {
+        public Guid Id { get; private init; }
+
+        public Entity(Guid id) {
+            Id = id;
+        }
+
+        public override bool Equals(object? obj) {
+            if (obj == this) {
+                return true;
+            }
+
+            if(obj is null) {
+                return false;
+            }
+
+            if(obj.GetType() != GetType()) {
+                return false;
+            }
+
+            return Id == ((Entity)obj).Id;
+        }
+
+        public bool Equals(Entity? other) {
+            if(other == this) {
+                return true;
+            }
+
+            if (other is null) {
+                return false;
+            }
+
+            if (other.GetType() != GetType()) {
+                return false;
+            }
+
+            return Id == other.Id;
+        }
+    }
+}
