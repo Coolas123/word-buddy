5aa4c72d8bb4a42d85c2267df91f7b8250b03574
Anton <gluxow.anton2015@yandex.ru>
2024-12-11 15:15:01 +0300

    Создана команда для создания строки словаря

---
+++ Application/Dictionaries/Commands/CreateDictionaryRow/CreateDictionaryRowCommand.cs
@@ -0,0 +1,12 @@
+﻿using Application.Abstractions.Messaging;
+using Application.Translations.Commands;
+using Application.Words.Commands.CreateWord;
+
+namespace Application.Dictionaries.Commands.CreateDictionaryRow
+{
+    public sealed class CreateDictionaryRowCommand : ICommand
+    {
+        public CreateWordCommand CreateWordCommand { get; set; } = new();
+        public CreateTranslationCommand CreateTranslationCommand { get; set; } = new();
+    }
+}
