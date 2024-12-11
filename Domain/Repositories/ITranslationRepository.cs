913cb3a5d8158af97a7a27f0333d7a6792673120
Anton <gluxow.anton2015@yandex.ru>
2024-12-11 15:16:43 +0300

    Доабвлены репозитории

---
+++ Domain/Repositories/ITranslationRepository.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Entities;
+
+namespace Domain.Repositories
+{
+    public interface ITranslationRepository : IBaseRepository<Translation>
+    {
+        bool CreateRange(IEnumerable<Translation> words);
+    }
+}
---
+++ Domain/Repositories/IWordRepository.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Entities;
+
+namespace Domain.Repositories
+{
+    public interface IWordRepository : IBaseRepository<Word>
+    {
+        bool CreateRange(IEnumerable<Word> words);
+    }
+}
