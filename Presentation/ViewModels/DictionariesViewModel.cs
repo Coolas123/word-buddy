eb43884d1920fe0ec04d6b38545775d2c99732a9
Anton <gluxow.anton2015@yandex.ru>
2024-12-12 17:56:51 +0300

    Удалены ненужные модели представления

---
+++ Presentation/ViewModels/DictionariesViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities;
+using Domain.TagHelperModels;
+
+namespace Presentation.ViewModels
+{
+    public sealed class DictionariesViewModel
+    {
+        public IEnumerable<Dictionary> Dictionaries { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
