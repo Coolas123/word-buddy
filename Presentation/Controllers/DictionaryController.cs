9d6a937bf170257404d30489321101ee28356791
Anton <gluxow.anton2015@yandex.ru>
2024-12-12 17:55:55 +0300

    Доабвлены методы контроллера для изменения и просмотра словаря

--- Presentation/Controllers/DictionaryController.cs
+++ Presentation/Controllers/DictionaryController.cs
@@ -1,35 +1,70 @@
-﻿using Domain.Shared;
+﻿using Application.Dictionaries.Commands.CreateDictionaryRow;
+using Application.Dictionaries.Commands.UpdateDictionary;
+using Application.Dictionaries.Commands.UpdateDictionaryAndRows;
+using Application.Dictionaries.Commands.UpdateDictionaryRow;
+using Application.Dictionaries.Queries.GetDictionary;
+using Application.Dictionaries.Queries.GetPagingDictionaries;
+using Application.Translations.Commands.UpdateTranslation;
+using Application.Words.Commands.UpdateWord;
+using Application.Words.Commands.UpdateWord.UpdateWords;
+using Domain.Shared;
+using Domain.TagHelperModels;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Presentation.Controllers.Extensions;
 using Presentation.ViewModels;
+using System.Reflection;
+using System.Security.Claims;
 
 namespace Presentation.Controllers
 {
+
     public class DictionaryController : Controller
     {
-        private readonly ISender Sender;
+        private int PageSize = 8;
+
+        private readonly ISender sender;
         public DictionaryController(ISender sender) {
-            Sender = sender;
+            this.sender = sender;
         }
         [Authorize(Roles="User")]
-        public IActionResult Index() {
-            return View();
+        public async Task<IActionResult> Index(int currentPage=1) {
+            var dictionariesResult = await sender.Send(new GetPagingDictionariesQuery
+            {
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = currentPage,
+                    ItemsPerPage = PageSize
+                },
+                UserId = Guid.Parse(User.FindFirstValue("Id"))
+            });
+
+            if (dictionariesResult.IsSuccess) {
+                var (pagingInfo, dictionaries) = dictionariesResult.Value();
+
+                return View(new DictionariesViewModel
+                {
+                    Dictionaries = dictionaries,
+                    PagingInfo = pagingInfo
+                });
+            }
+
+            return View(new DictionariesViewModel());
         }
 
         [Authorize(Roles = "WordLearner")]
         [HttpGet]
         public IActionResult CreateDictionary() {
-            return View(new CreateDictionaryViewModel());
+            return View(new CreateDictionaryAndRowsCommand());
         }
 
         [Authorize(Roles = "WordLearner")]
         [HttpPost]
-        public async Task<IActionResult> CreateDictionary(CreateDictionaryViewModel model) {
+        public async Task<IActionResult> CreateDictionary(CreateDictionaryAndRowsCommand model) {
             model.CreateDictionaryCommand.CreatedAt = DateTime.Now.ToUniversalTime();
-            
-            var CreateDictionaryResult = await Sender.Send(model.CreateDictionaryCommand);
+
+            var CreateDictionaryResult = await sender.Send(model);
             
             if (!CreateDictionaryResult.IsSuccess) {
                 var validationError = (IValidationResult)CreateDictionaryResult;
@@ -40,15 +75,61 @@ namespace Presentation.Controllers
                 TempData["DictionaryCreated"] = "Словарь создан";
             }
 
-            model.CreateDictionaryRowCommand.CreateWordCommand.DictionaryId = CreateDictionaryResult.Value();
+            return Redirect("Index");
+        }
 
-            var createDictionaryRowResult = await Sender.Send(model.CreateDictionaryRowCommand);
+        [Authorize(Roles = "WordLearner")]
+        [HttpGet("{dictionaryId:Guid}")]
+        public async Task<IActionResult> GetDictionary(Guid dictionaryId) {
+            var dictionaryResult = await sender.Send(new GetDictionaryQuery { DictionaryId= dictionaryId });
 
-            if (createDictionaryRowResult.IsFailure) {
-                var validationError = (IValidationResult)createDictionaryRowResult;
-                ModelState.AddModelError(validationError.Errors);
-                return View(model);
+            if (dictionaryResult.IsSuccess) {
+                var viewWords = new List<UpdateWordCommand>(dictionaryResult.Value().Words.Count);
+
+                foreach (var word in dictionaryResult.Value().Words) {
+                    viewWords.Add(new UpdateWordCommand
+                    {
+                        Id = word.Id,
+                        Text = word.Text,
+                        LearnStatus = word.LearnStatus,
+                        LearnStatusChangedAt = word.LearnStatusChangedAt,
+                        Translation = new UpdateTranslationCommand
+                        {
+                            WordId = word.Translation.WordId,
+                            Text = word.Translation.Text,
+                        }
+                    });
+                }
+
+                return View(new UpdateDictionaryAndRowsCommand
+                {
+                    UpdateDictionaryCommand = new UpdateDictionaryCommand
+                    {
+                        Title = dictionaryResult.Value().Title,
+                        Description = dictionaryResult.Value().Description,
+                        LastViewedAt = dictionaryResult.Value().LastViewedAt,
+                        WordLanguage = dictionaryResult.Value().WordLanguage,
+                        TranslationLanguage = dictionaryResult.Value().TranslationLanguage,
+                        DictionaryId = dictionaryResult.Value().Id
+                    },
+                    UpdateWordsCommand = new UpdateWordsCommand
+                    {
+                        Words = viewWords,
+                        DictionaryId = dictionaryResult.Value().Id
+                    }
+                });
             }
+
+            return Redirect("Index");
+        }
+
+        [Authorize(Roles = "WordLearner")]
+        [HttpPost]
+        public async Task<IActionResult> UpdateDictionary(UpdateDictionaryAndRowsCommand model) {
+            model.UpdateDictionaryCommand.LastViewedAt = DateTime.Now.ToUniversalTime();
+            model.UpdateDictionaryCommand.UserId = Guid.Parse(User.FindFirstValue("Id"));
+            var dictionaryResult = await sender.Send(model);
+
 
             return Redirect("Index");
         }
